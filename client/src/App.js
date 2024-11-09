248c10721ed4a1b3b2ef669165a74ba2c81dba9e
prajwal07 <prajwal.nimbalkar1910@gmail.com>
2024-11-09 23:40:32 +0530

    added assets

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios.get('/api/test')  
+      .then(response => console.log(response.data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>EZResume Frontend on Port 8000</h1>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -38,6 +38,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port -------> ${PORT}`);
 });
-app.get('/',(req,res)=>{
+app.get('/api/test',(req,res)=>{
    res.send("running")
 })
