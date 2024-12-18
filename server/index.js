42cff2e2de49aea32063fbbbacbc8c08b3fffd69
prajwal07 <prajwal.nimbalkar1910@gmail.com>
2024-12-19 00:35:31 +0530

    removing redudant files

--- server/index.js
+++ server/index.js
@@ -26,6 +26,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
 // Route middleware
 app.use('/users', userRoutes);
 app.use('/resumes', resumeRoutes);
