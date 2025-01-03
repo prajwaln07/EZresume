c66999be4bf510ad51a5957a7fb72f984f01f100
prajwal07 <prajwal.nimbalkar1910@gmail.com>
2025-01-04 01:30:09 +0530

    fixing bug

--- server/controllers/templateController.js
+++ server/controllers/templateController.js
@@ -65,12 +65,7 @@ exports.updateTemplate = async (req, res) => {
 
         if (thumbnail) {
             const cloudResponse = await uploadThumbnail(thumbnail.buffer);
-            if (cloudResponse && cloudResponse.secure_url) {
-                template.image = cloudResponse.secure_url; // Use only the URL
-                console.log("-->",secure_url);
-            } else {
-                throw new Error("Failed to upload thumbnail to Cloudinary");
-            }
+            template.image = cloudResponse.secure_url;
         }
         
         template.updatedAt=Date.now();
