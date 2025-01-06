6b51c11d3a79c1a8f2dfc1a2aec148ed604ef739
prajwal07 <prajwal.nimbalkar1910@gmail.com>
2025-01-07 01:21:13 +0530

    updated education data of dummy data

--- client/src/componemts/resume/components/data.jsx
+++ client/src/componemts/resume/components/data.jsx
@@ -27,26 +27,27 @@ export default{
             'CreatedRESTfulAPIs withNode.js and Express and improved backend speed.'
         },
     ],
-    education:[
+    education: [
         {
-            id:1,
-            universityName:'Western Illinois University',
-            startDate:'01-02-2023',
-            endDate:'01-02-2024',
-            degree:'Master',
-            major:'Computer Science',
-            description:''
+          id: 1,
+          universityName: 'Western Illinois University',
+          startDate: '01-02-2023',
+          endDate: '01-02-2024',
+          degree: 'Master',
+          major: 'Computer Science',
+          description: ''
         },
         {
-            id:2,
-            universityName:'Western Illinois University',
-            startDate:'01-03-2023',
-            endDate:'01-01-2025',
-            degree:'Master',
-            major:'Computer Science',
-            description:''
+          id: 2,
+          universityName: 'Western Illinois University',
+          startDate: '01-03-2023',
+          endDate: '01-01-2025',
+          degree: 'Bachelor',
+          major: 'Information Technology',
+          description: ''
         }
-    ],
+      ],
+      
     skills:[
         {
             id:1,
