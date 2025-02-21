4bf15dfa3a4270e239153273d01962a2ead9cca6
prajwal07 <prajwal.nimbalkar1910@gmail.com>
2025-02-21 14:58:44 +0530

    improved readability of templates schema

--- server/models/template.js
+++ server/models/template.js
@@ -1,16 +1,52 @@
 const mongoose = require('mongoose');
 
 const templateSchema = new mongoose.Schema({
-  name: { type: String, required: true, maxLength: 100 },
-  description: { type: String, required: true, maxLength: 5500 },
-  layout: { type: String, required: true }, 
-  isCustomizable: { type: Boolean, required: true, default: false },
-  categories: [{ type: String, enum: ['single-column', 'double-column', 'minimal', 'creative', 'professional'] }], 
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-  deletedAt: { type: Date, default: null }, 
-  image: { type: String, required: true }, 
-  downloads: { type: Number, default: 0 },
+   name: { 
+      type: String, 
+      required: true,
+      maxLength: 100 
+    },
+    description: { 
+      type: String,
+      required: true,
+      maxLength: 5500 
+    },
+    layout: {
+      type: String,
+      required: true 
+    }, 
+     isCustomizable: {
+       type: Boolean,
+       required: true,
+       default: false 
+      },
+
+  categories: [
+     { 
+      type: String,
+      enum: ['single-column', 'double-column', 'minimal', 'creative', 'professional']
+     }
+    ], 
+  createdAt: {
+      type: Date,
+      default: Date.now
+     },
+  updatedAt: {
+     type: Date,
+     default: Date.now
+     },
+  deletedAt: {
+     type: Date,
+      default: null
+     }, 
+  image: { 
+    type: String, 
+    required: true
+   }, 
+  downloads: {
+     type: Number,
+      default: 0
+     },
 });
 
 const Template = mongoose.model('Template', templateSchema);
