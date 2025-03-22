e4a445a862037ccb4b69906ea5ea3dcdcd95742c
prajwal07 <prajwal.nimbalkar1910@gmail.com>
2025-03-22 11:09:26 +0530

    removed unnessary logs

--- client/src/components/pages/admin/AdminDashboard.jsx
+++ client/src/components/pages/admin/AdminDashboard.jsx
@@ -109,7 +109,6 @@ const AdminDashboard = () => {
       try {
         const monthlyData = await Promise.all(
           months.map(async ({ month }) => {
-                console.log(month);
             const response = await axios.get(apiConfig.downloads.monthly(month), {
               withCredentials: true,
             });
