60f434bdd88af6bc6cf1ce3f8f6ffa35b223c7c8
prajwaln07 <142973627+prajwaln07@users.noreply.github.com>
2025-04-08 01:43:03 +0530

    Update resumeRoutes.js

--- server/routes/resumeRoutes.js
+++ server/routes/resumeRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const {getSuggestions} = require('../controllers/resumeController');
+const router = express.Router();
+
+router.post('/suggestion',getSuggestions);
+module.exports = router;
+
+
 
