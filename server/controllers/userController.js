db7017e2c0bf5290d5bf11e8362e3f6f584acacf
prajwaln07 <142973627+prajwaln07@users.noreply.github.com>
2025-04-07 22:38:13 +0530

    Update userController.js
    removed unused controllers

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -117,22 +117,6 @@ const loginUser = async (req, res) => {
 
 
 
-// Get User Profile
-const getUserProfile = async (req, res) => {
-  try {
-    const user = await User.findById(req.user.userId).select('-password');
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ message: 'Server error', error: error.message });
-  }
-};
-
-
-
-
 
 
 
@@ -236,7 +220,6 @@ const contactUs = async (req, res) => {
 module.exports = {
   registerUser,
   loginUser,
-  getUserProfile,
   deleteUserAccount,
   getUserCount,
   logoutUser,
