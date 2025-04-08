ffe2ccc94aac102fba804ed21cc07e4769c04546
prajwaln07 <142973627+prajwaln07@users.noreply.github.com>
2025-04-08 17:57:10 +0530

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,26 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // Default to localStorage
-import rootReducer from './reducers'; // Combine your reducers here
+import rootReducer from './reducers'; 
 import { combineReducers } from 'redux';
 
 // Persist configuration
 const persistConfig = {
   key: 'root',
-  storage,  // Specifies localStorage for persistence
-  whitelist: ['user','theme','template'],  // Persist only the 'user' reducer (authentication-related state)
+  storage,  
+  whitelist: ['user','theme','template'],  // the features that i want to persist
 };
 
-// Create persisted reducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// Create store
 const store = configureStore({
   reducer: persistedReducer,
-  devTools: process.env.NODE_ENV !== 'production', // Optional: Enable Redux DevTools in development
+  devTools: process.env.NODE_ENV !== 'production', 
 });
 
-// Create a persistor
 const persistor = persistStore(store);
 
 export { store, persistor };
