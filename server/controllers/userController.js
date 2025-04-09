8caed8d050702d0b97423111e3e973ba35a679df
prajwaln07 <142973627+prajwaln07@users.noreply.github.com>
2025-04-09 08:51:57 +0530

    Update userController.js
    token added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -105,6 +105,7 @@ const loginUser = async (req, res) => {
       )
       .json({
        success:true,
+        token=token,
        message: 'Login successful.',
        user: userData 
       });
